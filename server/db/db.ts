3ab4ffc92719c2f475d4589233cc9afd2f7349aa
thu.nguyen <thu.nguyen@easycredit.vn>
2018-03-23 21:24:20 +0700

    fix 11

--- server/db/db.ts
+++ server/db/db.ts
@@ -5,9 +5,9 @@ import * as dotenv from "dotenv";
 process.env.NODE_ENV = "product";
 dotenv.config({ path: ".env." + process.env.NODE_ENV });
 
-// const dbUrl: string = process.env.PG_URI;
-const dbUrl = "postgres://oauth2:oauth2MNL@13.250.129.169:5432/oauth2";
-
+const dbUrl: string = process.env.PG_URI;
+// const dbUrl = "postgres://oauth2:oauth2MNL@13.250.129.169:5432/oauth2";
+console.log("Load DB ===");
 const options: LoggingOptions = { benchmark: true, logging: console.log };
 export const sequelize: Sequelize = new ORM(dbUrl, options);
 
